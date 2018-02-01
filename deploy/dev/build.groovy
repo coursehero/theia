52683ca34e0bd60cc68fcd3d64dcd1951c6469d6
Connor Clark <connor.clark@coursehero.com>
2018-02-01 14:34:40 -0800

    add bin/cmd

--- deploy/dev/build.groovy
+++ deploy/dev/build.groovy
@@ -3,19 +3,11 @@ try{
         stage 'Checkout'
             git url: 'git@prod-git.coursehero.com:coursehero/service/theia.git', branch: BRANCH
 
-        // stage 'Ready Parameters'
-        //     sh "touch /var/filecabinet/devops/shared/dev-service-study-guide-data/PARAMS_BEING_USED"
-        //     sh "cp /var/filecabinet/devops/shared/dev-service-study-guide-data/parameters.yml config/parameters.yml"
-
         stage 'Yarn Install'
             sh 'docker run --rm -v ~/.ssh:/root/.ssh -v $(pwd):/var/build -w /var/build node:carbon /bin/bash -c "yarn install"'
 
         stage 'Yarn Build'
             sh 'docker run --rm -v ~/.ssh:/root/.ssh -v $(pwd):/var/build -w /var/build node:carbon /bin/bash -c "yarn run build"'
-
-        // stage 'Save Parameters'
-        //     sh "cp config/parameters.yml /var/filecabinet/devops/shared/dev-service-study-guide-data/parameters.yml"
-        //     sh "rm -f /var/filecabinet/devops/shared/dev-service-study-guide-data/PARAMS_BEING_USED"
 
         stage 'Tests'
             sh 'docker run --rm -v ~/.ssh:/root/.ssh -v $(pwd):/var/build -w /var/build node:carbon /bin/bash -c "yarn run test"'
