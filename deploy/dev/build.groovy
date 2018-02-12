c88c3ed70903be506ffea6a76cb2799df7e3cfb3
Connor Clark <connor.clark@coursehero.com>
2018-02-12 11:45:53 -0800

    try setting env in first docker instance

--- deploy/dev/build.groovy
+++ deploy/dev/build.groovy
@@ -5,8 +5,7 @@ try{
 
         // until Jenkin's docker is >=17.05, use dockerception
         stage 'Build / Test'
-            sh 'export DOCKER_API_VERSION=1.22'
-            sh 'docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/theia -w /theia docker:18 \
+            sh 'docker run --rm -e DOCKER_API_VERSION=1.22 -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/theia -w /theia docker:18 \
                   docker build \
                   --build-arg node_env=development \
                   -t 315915642113.dkr.ecr.us-east-1.amazonaws.com/dev-theia .'
