3cecd2bf61e17fa6a2d4ec5df81bb9adf2eaca2d
Kevin Wilde <kevin.wilde@coursehero.com>
2018-02-22 15:13:32 -0800

    chess-221 add secret for sqs queue url

--- deploy/secrets.sh
+++ deploy/secrets.sh
@@ -2,6 +2,7 @@
 
 export THEIA_AUTH_SECRET=$(cat /run/secrets/api.authKey)
 export THEIA_ROLLBAR_TOKEN=$(cat /run/secrets/theia.rollbar.token)
+export THEIA_SQS_REHEAT_CACHE_URL=$(cat /run/secrets/theia.sqs.queue)
 
 echo "[default]" >> /root/.aws/credentials
 echo -n "aws_access_key_id = " >> /root/.aws/credentials
