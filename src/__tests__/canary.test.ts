5e31a751b47dff25ab63878cbb8dc464232adc19
Connor Clark <connor.clark@coursehero.com>
2018-03-04 13:08:30 -0800

    test for expected build output and render request

--- src/__tests__/canary.test.ts
+++ src/__tests__/canary.test.ts
@@ -1,11 +1,8 @@
+import * as rimraf from 'rimraf'
 import Core from '../core'
 import LocalStorage from '../local-storage'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
-})
-
-test('builds mythos', () => {
+describe('mythos', () => {
   const config: Theia.Configuration = {
     libs: {
       "mythos": {
@@ -18,6 +15,7 @@ test('builds mythos', () => {
     }
   }
 
+  rimraf.sync(__dirname + '/jest-libs')
   const storage = new LocalStorage(__dirname + '/jest-libs')
 
   const theia = new Core({
@@ -25,7 +23,38 @@ test('builds mythos', () => {
     storage
   })
 
-  return theia.buildAll().then(() => {
-    console.log('done with test build')
+  beforeAll(() => theia.buildAll(), 1000 * 60)
+  afterAll(() => rimraf.sync(__dirname + '/jest-libs'))
+  
+  test('saves build manifest and assets', async () => {
+    const buildManifest = await theia.getBuildManifest('mythos')
+    expect(buildManifest.length).toBe(1)
+    
+    const statsBasename = buildManifest[0].stats
+    const stats = JSON.parse(await theia.storage.load('mythos', statsBasename))
+    expect(stats).toMatchObject({
+      "assetsByChunkName": {
+        "manifest": [
+          "manifest.19921ef415ad5c4fdaf5.js",
+          "manifest.19921ef415ad5c4fdaf5.js.map"
+        ]
+      }
+    })
+    
+    for (const assetBasename of stats.assetsByChunkName.manifest) {
+      const assetSource = await theia.storage.load('mythos', assetBasename)
+      expect(assetSource).toBeTruthy()
+    }
   })
-}, 1000 * 60)
+
+  test('renders', async () => {
+    const result = await theia.render('mythos', 'Greeting', { name: 'Theia' })
+    expect(result).toEqual({
+      html: '<div data-reactroot="">Hello <em>Theia</em>!!!</div>',
+      assets: {
+        javascripts: [ 'manifest.19921ef415ad5c4fdaf5.js' ],
+        stylesheets: []
+      }
+    })
+  })
+})
