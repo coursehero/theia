ab854337e08948a39aee72623607186d1f229175
Peter Woo <pwoo@coursehero.com>
2018-07-05 02:46:31 -0700

    Adding --privileged flag in case it enables us to clear git workspace

--- deploy/dev/build.groovy
+++ deploy/dev/build.groovy
@@ -8,7 +8,7 @@ try{
         stage 'Checkout'
             if("true" == "${FORCE_CLEAR_GIT_WORKSPACE}") {
               echo "Force clearing Git workspace"
-              sh 'docker run --rm --net="host" -v /var/lib/jenkins/jobs/Build-DevTheia:/root/Build-DevTheia:rw -i 315915642113.dkr.ecr.us-east-1.amazonaws.com/scriptbox:php7.1 rm -fr /root/Build-DevTheia/workspace/*'
+              sh 'docker run --privileged --rm --net="host" -v /var/lib/jenkins/jobs/Build-DevTheia:/root/Build-DevTheia:rw -i 315915642113.dkr.ecr.us-east-1.amazonaws.com/scriptbox:php7.1 rm -fr /root/Build-DevTheia/workspace/*'
             }
             git url: 'git@prod-git.coursehero.com:coursehero/service/theia.git', branch: BRANCH
 
