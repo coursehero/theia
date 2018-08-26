9f0809910e64990655faaf09d724fb7170db7aeb
Connor Clark <connor.clark@coursehero.com>
2018-08-25 21:59:59 -0700

    fix views, public express folders

--- plugins/usage/UsagePlugin.ts
+++ plugins/usage/UsagePlugin.ts
@@ -1,11 +1,19 @@
 import { Core, CoreHooks, Plugin } from '@coursehero/theia'
 
+function appendToViews (oldViews: string[] | string | undefined, newView: string) {
+  if (Array.isArray(oldViews)) return [...oldViews, newView]
+  if (oldViews) return [oldViews, newView]
+  return [newView]
+}
+
 class UsagePlugin implements Plugin {
   apply (core: Core) {
     core.hooks.express.tapPromise('UsagePlugin', this.onExpress)
   }
 
   onExpress = ({ core, app }: CoreHooks.OnExpressArgs) => {
+    app.set('views', appendToViews(app.get('views'), __dirname + '/views'))
+
     app.get('/', async (req, res) => {
       const helloWorldResult = await core.render(req, '@coursehero/mythos', 'Greeting', {
         name: 'World'
